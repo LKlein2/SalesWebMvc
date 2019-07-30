51d611230e321b37c6c9d79b8ed4f564b4dd8d20
Lucas klein <Lucas.Klein@live.com>
2019-07-30 20:51:53 -0300

    Tudo "pronto" antes da implentação de metodos assincronos

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,23 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
+        [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres")]
         public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
         public string Email { get; set; }
+
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} obrigatória")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Salario base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Range(998.0, 5000.00, ErrorMessage = "{0} deve ser maior que 998.00 e menor que 5000.00")]
         public double BaseSalary { get; set; }
+
+        [Display(Name = "Departamento")]
         public Department Department { get; set; }
+
+        [Display (Name="Departamento")]
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
-
         }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
